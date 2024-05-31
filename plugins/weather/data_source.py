665835bd2961aa78ca1b41738e1185252dbb7b12
xiaobaozi233 <68990805+xiaobaozi233@users.noreply.github.com>
2024-05-31 14:03:17 +0800

    Update data_source.py
    修复天气查询（https无法使用)
    添加自定义趣味天气查询

--- plugins/weather/data_source.py
+++ plugins/weather/data_source.py
@@ -1,79 +1,76 @@
-from utils.message_builder import image
+from nonebot import on_regex
+from .data_source import get_weather_of_city, get_city_list
+from nonebot.adapters.onebot.v11 import MessageEvent, GroupMessageEvent
+from jieba import posseg
+from services.log import logger
+from nonebot.params import RegexGroup
+from typing import Tuple, Any
+import json
 from configs.path_config import TEXT_PATH
-from configs.config import NICKNAME
-from typing import List
-from nonebot import Driver
-from utils.http_utils import AsyncHttpx
-import ujson as json
-import nonebot
+weather_descriptions = TEXT_PATH / "weather_descriptions.json"
 
-driver: Driver = nonebot.get_driver()
-
-china_city = TEXT_PATH / "china_city.json"
-
-data = {}
-
-
-async def get_weather_of_city(city: str) -> str:
-    """
-    获取城市天气数据
-    :param city: 城市
-    """
-    code = _check_exists_city(city)
-    if code == 999:
-        return "不要查一个省份的天气啊，很累人的！"
-    elif code == 998:
-        return f"{NICKNAME}没查到!!试试查火星的天气？"
-    else:
-        data_json = (
-            await AsyncHttpx.get(
-                f"https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city={city[:-1]}"
-            )
-        ).json()
-        if wh := data_json.get('data'):
-            w_type = wh[0]["wea_day"]
-            w_max = wh[0]["tem1"]
-            w_min = wh[0]["tem2"]
-            fengli = wh[0]["win_speed"]
-            ganmao = wh[0]["narrative"]
-            fengxiang = ','.join(wh[0].get('win', []))
-            repass = f"{city}的天气是 {w_type} 天\n最高温度: {w_max}\n最低温度: {w_min}\n风力: {fengli} {fengxiang}\n{ganmao}"
-            return repass
-        else:
-            return data_json.get("errmsg") or "好像出错了？再试试？"
+__zx_plugin_name__ = "天气查询"
+__plugin_usage__ = """
+usage：
+    普普通通的查天气吧
+    指令：
+        [城市]天气
+""".strip()
+__plugin_des__ = "出门要看看天气，不要忘了带伞"
+__plugin_cmd__ = ["[城市]天气/天气[城市]"]
+__plugin_type__ = ("一些工具",)
+__plugin_version__ = 0.1
+__plugin_author__ = "HibiKier"
+__plugin_settings__ = {
+    "level": 5,
+    "default_status": True,
+    "limit_superuser": False,
+    "cmd": ["查询天气", "天气", "天气查询", "查天气"],
+}
 
 
-def _check_exists_city(city: str) -> int:
-    """
-    检测城市是否存在合法
-    :param city: 城市名称
-    """
-    global data
-    city = city if city[-1] != "市" else city[:-1]
-    for province in data.keys():
-        for city_ in data[province]:
-            if city_ == city:
-                return 200
-    for province in data.keys():
-        if city == province:
-            return 999
-    return 998
+weather = on_regex(r".{0,10}?(.*)的?天气.{0,10}", priority=5, block=True)
+def load_weather_descriptions(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return json.load(file)
 
+@weather.handle()
+async def _(event: MessageEvent, reg_group: Tuple[Any, ...] = RegexGroup()):
+    msg = reg_group[0]
+    
+    weather_descriptions_data = load_weather_descriptions(weather_descriptions)
 
-def get_city_list() -> List[str]:
-    """
-    获取城市列表
-    """
-    global data
-    if not data:
-        try:
-            with open(china_city, "r", encoding="utf8") as f:
-                data = json.load(f)
-        except FileNotFoundError:
-            data = {}
-    city_list = []
-    for p in data.keys():
-        for c in data[p]:
-            city_list.append(c)
-        city_list.append(p)
-    return city_list
+    if msg in weather_descriptions_data:
+        await weather.finish(weather_descriptions_data[msg])
+        return
+
+    if msg and msg[-1] != "市":
+        msg += "市"
+
+    if msg in weather_descriptions_data:
+        await weather.finish(weather_descriptions_data[msg])
+        return
+
+    city = ""
+    if msg:
+        city_list = get_city_list()
+        words = posseg.lcut(msg)
+        found = False
+        for word in words:
+            key = word.word
+            if key in weather_descriptions_data:
+                await weather.finish(weather_descriptions_data[key])
+                found = True
+                break
+            if not found and (word.flag == "ns" or word.word[:-1] in city_list):
+                city = str(word.word).strip()
+                break
+    
+    if city:
+        city_weather = await get_weather_of_city(city)
+        logger.info(
+            f'(USER {event.user_id}, GROUP {event.group_id if isinstance(event, GroupMessageEvent) else "private"} ) '
+            f"查询天气:" + city
+        )
+        await weather.finish(city_weather)
+
